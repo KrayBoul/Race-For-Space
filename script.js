ef4a0544806b0cee096fa8fb242fbc50549f0526
KrayBoul <66803875+KrayBoul@users.noreply.github.com>
2022-07-01 02:13:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 
 // Key definition
-let W = false; // 87
-let A = false; // 65
-let S = false; // 83
-let D = false; // 68
+let UP = false; // 38
+let LEFT = false; // 37
+let DOWN = false; // 40
+let RIGHT = false; // 39
 let canvas;
 let ctx;
-let backgroundImage = new Image();
 
+//Music
+let audio = document.getElementById("audio");
+
+
+
+let background = {
+    x: 0,
+    y: -350,
+    width: 500,
+    height: 500,
+    src:'Bilder/background.jpg'
+}
 
 let spaceship = {
     x: 135,
@@ -19,7 +30,7 @@ let spaceship = {
 
 let warship1 = {
     x: -90,
-    y: -100,
+    y: -300,
     width: 150,
     height: 400,
     src:'Bilder/warship1.png'
@@ -27,7 +38,7 @@ let warship1 = {
 
 let warship2 = {
     x: 240,
-    y: -100,
+    y: -300,
     width: 150,
     height: 400,
     src:'Bilder/warship2.png'
@@ -35,33 +46,33 @@ let warship2 = {
 
 // Keys pressed
 window.onkeydown = function(e) {
-    if (e.keyCode == 87) { 
-        W = true;
+    if (e.keyCode == 38) { 
+        UP = true;
     }
-    if (e.keyCode == 65) { 
-        A = true;
+    if (e.keyCode == 37) { 
+        LEFT = true;
     }
-    if (e.keyCode == 83) { 
-        S = true;
+    if (e.keyCode == 40) { 
+        DOWN = true;
     }
-    if (e.keyCode == 68) { 
-        D = true;
+    if (e.keyCode == 39) { 
+        RIGHT = true;
     }
 }
 
 // Keys not pressed
 window.onkeyup = function(e) {
-    if (e.keyCode == 87) { 
-        W = false;
+    if (e.keyCode == 38) { 
+        UP = false;
     }
-    if (e.keyCode == 65) { 
-        A = false;
+    if (e.keyCode == 37) { 
+        LEFT = false;
     }
-    if (e.keyCode == 83) { 
-        S = false;
+    if (e.keyCode == 40) { 
+        DOWN = false;
     }
-    if (e.keyCode == 68) { 
-        D = false;
+    if (e.keyCode == 39) { 
+        RIGHT = false;
     }
 }
 
@@ -74,18 +85,55 @@ function startGame() {
 }
 
 function update() {
-    if (D) {
-        spaceship.x -= 2;
+    if (LEFT) {
+        spaceship.x -= 3;
     }
 
-    if (D) {
-        spaceship.x += 2;
+    if (RIGHT) {
+        spaceship.x += 3;
+    }
+    if (UP) {
+        warship1.y += 0.5;
+        warship2.y += 0.5;
+        background.y += 2;
+    }
+
+    if (DOWN) {
+        warship1.y -= 0.5;
+        warship2.y -= 0.5;
+        background.y -= 0.5;
     }
 }
 
 
+//Sound and Audio
+let isPlaying = false;
+    audio.volume = 0.2;
+    audio.loop = true;
+
+function togglePlay() {
+    isPlaying ? audio.pause() : audio.play();
+}
+
+audio.onplaying = function() {
+    isPlaying = true;
+}
+
+audio.onpause = function() {
+    isPlaying = false;
+}
+
+startBtn.onClick = function() {
+	playSound.play();
+}
+
+restartBtn.onClick = function() {
+	playSound.play();
+}
+
 function loadImages () {
-    backgroundImage.src = 'Bilder/background.jpg';
+    background.img = new Image();
+    background.img.src = background.src;
     spaceship.img = new Image();
     spaceship.img.src = spaceship.src;
     warship1.img = new Image();
@@ -95,7 +143,7 @@ function loadImages () {
 }
 
 function draw() {
-    ctx.drawImage(backgroundImage, 0, 0);
+    ctx.drawImage(background.img, background.x, background.y, background.width, background.height);
     ctx.drawImage(warship1.img, warship1.x, warship1.y, warship1.width, warship1.height);
     ctx.drawImage(warship2.img, warship2.x, warship2.y, warship2.width, warship2.height);
     ctx.drawImage(spaceship.img, spaceship.x, spaceship.y, spaceship.width,  spaceship.height);
